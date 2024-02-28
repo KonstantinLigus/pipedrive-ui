883a0aab3d2564fa3a4aa6a00a7accd4b76347c0
Konstantin Ligus <ligus.konstantin@gmail.com>
2024-02-28 19:22:48 +0200

    initiate app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AppExtensionsSDK from "@pipedrive/app-extensions-sdk";
+
+const sdk = await new AppExtensionsSDK({ identifier: "123abc" }).initialize({
+  size: { height: 500 },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <label>
+      field
+      <input type="text" style={{ width: "300px", height: "50px" }} />
+    </label>
   );
 }
 
